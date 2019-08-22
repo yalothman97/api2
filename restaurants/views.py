79c8936f1f1f8ed9980f94de11651f0b113c86d1
Yousef Al Othman <Yousef@MacBook-Pro-2.local>
2019-08-22 15:22:12 +0300

    Hessah AlHamadi & Youssef AlOthman

--- restaurants/views.py
+++ restaurants/views.py
@@ -31,10 +31,19 @@ def restaurant_create(request):
     return render(request, 'create.html', context)
 
 def restaurant_update(request, restaurant_id):
-
-    return
+    restaurant = Restaurant.objects.get(id=restaurant_id)
+    form = RestaurantForm(instance=restaurant)
+    if request.method == "POST":
+        form = RestaurantForm(request.POST, instance=restaurant)
+        if form.is_valid():
+            form.save()
+            return redirect('restaurant-list')
+    context = {
+        "form":form,
+        "restaurant": restaurant
+    }
+    return render(request, "update.html", context)
 
 def restaurant_delete(request, restaurant_id):
-
-
-    return
+    Restaurant.objects.get(id=restaurant_id).delete()
+    return redirect("restaurant-list")
